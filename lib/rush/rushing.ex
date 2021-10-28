c11e5760de0157b64dfefe8c809bab78d0a78e77
David Dallaire <ddallaire@mirego.com>
2021-10-27 21:03:13 -0400

    Add search and a way to do paging and ordering

--- lib/rush/rushing.ex
+++ lib/rush/rushing.ex
@@ -8,7 +8,57 @@ defmodule Rush.Rushing do
   alias Rush.Repo
   alias Rush.Rushing.PlayerRush
 
-  def list_player_rushes do
-    Repo.all(PlayerRush)
+  def page_count(opts) do
+    opts
+    |> search_query()
+    |> limit(nil)
+    |> offset(nil)
+    |> Repo.aggregate(:count)
   end
+
+  def search_players(opts) do
+    opts
+    |> search_query()
+    |> Repo.all()
+  end
+
+  defp search_query(opts) do
+    PlayerRush
+    |> apply_ordering(opts)
+    |> apply_pagination(opts)
+    |> apply_filter(opts)
+  end
+
+  defp apply_ordering(query, %{ordering_field: key, ordering_direction: direction}) do
+    from(
+      player_rushes in query,
+      order_by: {^direction, ^key}
+    )
+  end
+
+  defp apply_ordering(query, _opts), do: query
+
+  defp apply_pagination(query, %{page: page} = opts) when not is_nil(page) do
+    page_size = Map.get(opts, :page_size, 10)
+
+    from(
+      player_rushes in query,
+      limit: ^page_size,
+      offset: ^((page - 1) * page_size)
+    )
+  end
+
+  defp apply_pagination(query, _opts), do: query
+
+  defp apply_filter(query, %{query: nil}), do: query
+  defp apply_filter(query, %{query: ""}), do: query
+
+  defp apply_filter(query, %{query: query_input}) do
+    from(
+      player_rushes in query,
+      where: fragment("searchable_player_name @@ to_tsquery(?)", ^prefix_search(query_input))
+    )
+  end
+
+  defp prefix_search(term), do: String.replace(term, ~r/\W/u, "") <> ":*"
 end
--- lib/rush_web/live/player_rush_live/index.ex
+++ lib/rush_web/live/player_rush_live/index.ex
@@ -2,11 +2,30 @@ defmodule RushWeb.PlayerRushLive.Index do
   use RushWeb, :live_view
 
   alias Rush.Rushing
-  alias Rush.Rushing.PlayerRush
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :player_rushes, list_player_rushes())}
+    {:ok, initial_assigns(socket)}
+  end
+
+  @impl true
+  def handle_event("search_by_name", %{"search_field" => %{"query" => query}}, socket) do
+    socket =
+      socket
+      |> assign(:query, query)
+      |> put_page_count()
+      |> put_player_rushes()
+
+    {:noreply, socket}
+  end
+
+  defp initial_assigns(socket) do
+    socket
+    |> assign(:page, 1)
+    |> assign(:page_size, 10)
+    |> assign(:query, nil)
+    |> assign(:ordering_field, :name)
+    |> assign(:ordering_direction, :asc)
   end
 
   @impl true
@@ -14,9 +33,25 @@ defmodule RushWeb.PlayerRushLive.Index do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  defp apply_action(socket, :index, _params), do: socket
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> put_page_count()
+    |> put_player_rushes()
+  end
 
-  defp list_player_rushes do
-    Rushing.list_player_rushes()
+  defp put_player_rushes(%{assigns: %{count: 0}} = socket) do
+    assign(socket, :player_rushes, [])
+  end
+
+  defp put_player_rushes(socket) do
+    assign(socket, :player_rushes, Rushing.search_players(socket.assigns))
+  end
+
+  defp put_page_count(socket) do
+    page_count = Rushing.page_count(socket.assigns)
+
+    socket
+    |> assign(:page_count, page_count)
+    |> assign(:page, min(socket.assigns.page, max(page_count, 1)))
   end
 end
